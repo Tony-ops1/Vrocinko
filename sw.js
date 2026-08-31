37fef233612504c41a3d81d2c12f7870bad56ce1
Tony-ops1 <toni.erste@gmail.com>
2026-08-31 15:02:02 +0200

    Osveži predpomnilnik za otroško ozadje

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='vrocinko-v12';
-const FILES=['./','./index.html','./style.css','./background.css','./background.js','./app.js','./manifest.json','./icon.svg','./assets/bg1.txt','./assets/bg2.txt','./assets/bg3.txt','./assets/bg4.txt','./assets/bg5.txt'];
+const CACHE='vrocinko-v13';
+const FILES=['./','./index.html','./style.css','./background.css','./background.js','./app.js','./manifest.json','./icon.svg','./assets/bgw1.txt','./assets/bgw2.txt','./assets/bgw3.txt','./assets/bgw4.txt','./assets/bgw5.txt','./assets/bgw6.txt'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));
