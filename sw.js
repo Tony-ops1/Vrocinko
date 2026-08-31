aa0fa7f45b176a0e5a287b11687e93dc080fd2bb
Tony-ops1 <toni.erste@gmail.com>
2026-08-31 13:49:28 +0200

    Refresh cache after illness day removal

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vrocinko-v10';
+const CACHE='vrocinko-v11';
 const FILES=['./','./index.html','./style.css','./app.js','./manifest.json','./icon.svg'];
 
 self.addEventListener('install',event=>{
