7358479b722c14116a111d0f951766862c0aae72
Tony-ops1 <toni.erste@gmail.com>
2026-08-31 10:02:22 +0200

    Osveži predpomnilnik po spremembi vnosa temperature

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vrocinko-v2';
+const CACHE='vrocinko-v3';
 const FILES=['./','./index.html','./style.css','./app.js','./manifest.json','./icon.svg'];
 
 self.addEventListener('install',event=>{
