ce1ecf75ddb9f0d19f5c90a5e5718dc62717b272
Tony-ops1 <toni.erste@gmail.com>
2026-08-31 12:47:19 +0200

    Osveži predpomnilnik za podporo več otrokom

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vrocinko-v8';
+const CACHE='vrocinko-v9';
 const FILES=['./','./index.html','./style.css','./app.js','./manifest.json','./icon.svg'];
 
 self.addEventListener('install',event=>{
