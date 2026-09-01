fa82775c8703c752414dc5e2099f65f8bcdb9caf
Tony-ops1 <toni.erste@gmail.com>
2026-09-01 10:42:29 +0200

    Posodobi predpomnilnik na v19

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vrocinko-v18';
+const CACHE='vrocinko-v19';
 const FILES=['./','./index.html','./style.css','./background.css','./background.js','./app.js','./edit.js','./days.js','./manifest.json','./icon.svg','./assets/bgw1.txt','./assets/bgw2.txt','./assets/bgw3.txt','./assets/bgw4.txt','./assets/bgw5.txt','./assets/bgw6.txt'];
 
 self.addEventListener('install',event=>{
